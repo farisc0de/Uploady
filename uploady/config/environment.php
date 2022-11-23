73649054dcf498f2879ee2a140cb253fdd55f009
Faris AL-Otaibi <farisksa79@gmail.com>
2022-11-23 14:58:28 +0300

    Updated ENVIRONMENT

--- uploady/config/environment.php
+++ uploady/config/environment.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ENVIRONMENT', 'development');
+define('ENVIRONMENT', 'installation');
 ini_set("memory_limit", "1024M");
 
 switch (ENVIRONMENT) {
@@ -13,7 +13,7 @@ switch (ENVIRONMENT) {
         ini_set('display_errors', 0);
         error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
         break;
-    case 'production':
+    case 'installation':
         $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
         $c =  (strpos($url, 'install.php'));
         if ($c == false) {
