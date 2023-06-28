d703d016268152d3b19bd2e0ecbecf8588990da3
Faris AL-Otaibi <farisksa79@gmail.com>
2023-06-29 01:38:14 +0300

    Adding back delete file and refactord the the includes

--- uploady/components/js.php
+++ uploady/components/js.php
@@ -1,15 +1,25 @@
 <?php
+
 $utils->script("https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js");
 $utils->script("https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js");
 $utils->script("https://code.jquery.com/jquery-3.6.0.min.js");
 $utils->script("https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js");
 $utils->script("https://cdn.datatables.net/v/bs5/dt-1.11.3/datatables.min.js");
-$utils->script("https://platform-api.sharethis.com/js/sharethis.js#property={$st['sharethis_code']}&product=inline-share-buttons");
+
+if (strpos($_SERVER['REQUEST_URI'], 'download.php') !== false) {
+    $utils->script("https://platform-api.sharethis.com/js/sharethis.js#property={$st['sharethis_code']}&product=inline-share-buttons");
+}
+
 $utils->script("https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone-min.js");
 $utils->script("https://cdn.datatables.net/fixedcolumns/4.2.2/js/dataTables.fixedColumns.min.js");
-$utils->script("vendor/camansjs/dist/caman.full.min.js");
+
+if (strpos($_SERVER['REQUEST_URI'], 'edit.php') !== false) {
+    $utils->script("vendor/camansjs/dist/caman.full.min.js");
+    $utils->script("js/editor.js");
+}
+
 $utils->script("js/main.js");
-$utils->script("js/editor.js");
 $utils->script("js/custom.js");
+
 include_once APP_PATH . "modules/adsense/main.php";
 include_once APP_PATH . "modules/analytics/main.php";
---
+++ uploady/delete.php
@@ -0,0 +1,24 @@
+<?php include_once 'session.php'; ?>
+
+<?php include_once 'logic/deleteLogic.php'; ?>
+
+<?php include_once 'components/header.php'; ?>
+
+<div class="container pb-5 pt-5">
+    <div class="row justify-content-center text-center">
+        <div class="col-sm-12 col-md-8 col-lg-8">
+            <div class="card">
+                <div class="card-header">
+                    <?= $lang['delete_file_title'] ?>
+                </div>
+                <div class="card-body">
+                    <div class="border border-primary bg-primary rounded">
+                        <p class="pt-3 text-light"><?= $msg; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include_once 'components/footer.php'; ?>
