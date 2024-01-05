80dd236840a2fb0ebfbcf04e82c9b6e4c74acd5a
Faris AL-Otaibi <farisksa79@gmail.com>
2024-01-05 19:12:03 +0300

    update permission to 755

--- uploady/config/config.php
+++ uploady/config/config.php
@@ -2,12 +2,12 @@
 
 // Database Settings
 define("DB_HOST", "localhost");
-define("DB_USER", "");
-define("DB_PASS", "");
-define("DB_NAME", "");
+define("DB_USER", "root");
+define("DB_PASS", "farisksa1997");
+define("DB_NAME", "uploady");
 
 // Application Settings
-define("SITE_URL", "http://localhost/uploady");
+define("SITE_URL", "http://localhost/Uploady/uploady");
 define("APP_PATH", dirname(__FILE__, 2) . DIRECTORY_SEPARATOR);
 define("LOGS_PATH", APP_PATH . "php_logs.log");
 
