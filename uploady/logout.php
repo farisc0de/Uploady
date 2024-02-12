47be13d023ca6a92b73d6350824f4681160b7bf5
Faris AL-Otaibi <farisksa79@gmail.com>
2024-02-13 00:31:40 +0300

    Fix a bug in logout.

--- uploady/logout.php
+++ uploady/logout.php
@@ -1,12 +1,13 @@
 <?php
 
 session_start();
+
 include_once 'config/config.php';
 
 $utils = new Uploady\Utils();
 
 if (session_destroy()) {
-    if ($_GET['redirect'] == "delete") {
+    if (isset($_GET['redirect']) && $_GET['redirect'] == "delete") {
         $utils->redirect($utils->siteUrl('/login.php?msg=delete'));
     }
 
